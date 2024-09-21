c8512e6017394a24e22d7854558478749a11cd7b
Somiyo <somiyomagar@gmail.com>
2024-09-20 23:42:50 -0600

    added deployment from github pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css'
 import Alert from './components/Alert'
-import About from './components/About'
+// import About from './components/About'
 import NavBaar from './components/NavBaar'
 import TextForm from './components/TextForm'
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [mode, setMode] = useState('light') // to check dark mode
@@ -36,26 +36,26 @@ function App() {
   }
   return (
     <>
-      <Router>
-        <NavBaar title="TextUtilis" mode={mode} toggleMode={toggleMode} />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Routes>
-            <Route exact path="/about" element={<About />} />
-            <Route
+      {/* <Router> */}
+      <NavBaar title="TextUtilis" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Routes> */}
+        {/* <Route exact path="/about" element={<About />} /> */}
+        {/* <Route
               exact
               path="/"
-              element={
-                <TextForm
-                  showAlert={showAlert}
-                  heading="Enter the text to analyze below"
-                  mode={mode}
-                />
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
+              element={ */}
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter the text to analyze below"
+          mode={mode}
+        />
+        {/* }
+            /> */}
+        {/* </Routes> */}
+      </div>
+      {/* </Router>   */}
     </>
   )
 }
